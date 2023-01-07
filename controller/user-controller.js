3e30e092e836c49fa91425ae1056935f6eec1325
Vinayaka-Doozie <vinayakadayanand007@gmail.com>
2023-01-07 15:42:19 +0530

    Login done

--- controller/user-controller.js
+++ controller/user-controller.js
@@ -1,6 +1,11 @@
 import User from '../model/user.js'
 // request sent by frontend, response - given back to frontend
 import bcrypt from 'bcrypt';
+import jwt  from 'jsonwebtoken';
+import dotenv from 'dotenv'
+import Token from '../model/token.js';
+
+dotenv.config();
 
 export const signupUser = async (request, response) => {
     try{
@@ -14,3 +19,33 @@ export const signupUser = async (request, response) => {
         return response.status(500).json({msg: error})
     }
 }
+
+export const loginUser = async (request, response) => {
+    let user = await User.findOne({email: request.body.email});
+    if(!user){
+        return response.status(400).json({msg: "Username doesn't exist"})
+    }
+    
+    try {
+        let match = bcrypt.compare(user.password, request.body.password);
+        if(match){
+            // secret key generated using 'crypto' module in node
+            // When access token expires, it takes the help of refresh token to generate a new token
+            const accessToken = jwt.sign(user.toJSON(), process.env.ACCESS_SECRET_KEY, {expiresIn : '15m'})
+            const refreshToken = jwt.sign(user.toJSON(), process.env.ACCESS_REFRESH_KEY) 
+
+            const newToken = new Token({token: refreshToken});
+            await newToken.save();
+
+            return response.status(200).json({ accessToken: accessToken, refreshToken: refreshToken, name: user.name, email: user.email})
+        }
+        else{
+            response.status(400).json({msg: "Password doesn't match"})
+        }
+    } catch (error) {
+        response.status(500).json({msg: "Error while logging in the userf"})
+
+    }
+}
+
+
